8b531d1a3aa370f3974fe01d7f28c254e1afea2e
satokofuwa <sato.fuwa@nifty.com>
2022-04-12 00:06:37 +0900

    Basic認証を導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+    private
+
+      def basic_auth
+        if Rails.env == "production"
+          authenticate_or_request_with_http_basic do |username, password|
+          username == ENV["MANYO_USER"] && password == ENV["MANYO_PASS"]
+        end
+      end
+  end
 end
